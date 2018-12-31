3243eb8103f8f9e80cef342454db518d2908d0ca
hemanth <hemanth.shendye@gmail.com>
2018-12-31 22:25:23 +0530

    weather json added

--- success/urls.py
+++ success/urls.py
@@ -18,11 +18,12 @@ from django.contrib import admin
 from django.contrib.auth import logout,urls
 from django.contrib.auth import authenticate, login
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ]
